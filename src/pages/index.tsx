7ef419f07cdb9217d50ffa836faf5c273ea782a5
gorazdKuzev12 <gorazd.kuzev10@gmail.com>
2023-07-12 13:12:20 +0200

    feat: Initial set up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,41 @@
 import React from "react";
 export default function Home() {
   return (
-    <div className="text-gray-400 text-center">
-      <h1 className="text-6xl font-bold mb-4">CodeQZ</h1>
-      <p className="text-lg">Welcome to my portfolio</p>
+    <div className="flex h-screen">
+      {/* Menu */}
+      <div className="w-1/10 bg-gray-900 shadow-lg flex flex-col justify-center items-center text-white">
+        <div className="text-xl font-bold mb-2">Gorazd Kuzev</div>
+        <div className="flex flex-col">
+          <a href="#" className="py-2">
+            About me
+          </a>
+          <a href="#" className="py-2">
+            Projects
+          </a>
+          <a href="#" className="py-2">
+            Blog
+          </a>
+          <a href="#" className="py-2">
+            Contact
+          </a>
+        </div>
+      </div>
+
+      {/* Main Content */}
+      <div className="w-9/10 bg-gray-800 flex flex-col justify-center items-center text-white">
+        <div className="flex items-center mb-4">
+          <div className="w-1/6 h-px bg-orange-500 mr-4"></div>
+          <div>
+            <div className="text-2xl font-bold">Gorazd Kuzev</div>
+            <div className="text-lg">Web Developer</div>
+          </div>
+        </div>
+        <img
+          src="/profile-pic.jpg"
+          className="w-1/3 h-auto rounded-full"
+          alt="Gorazd Kuzev profile picture"
+        />
+      </div>
     </div>
   );
 }
