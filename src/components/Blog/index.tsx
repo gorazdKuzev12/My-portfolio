6c5aabec362f3c1dfed72f84ddc565aa4010818c
gorazdKuzev12 <gorazd.kuzev10@gmail.com>
2023-07-18 14:27:46 +0200

    feat: Desing changes

--- src/components/Blog/index.tsx
+++ src/components/Blog/index.tsx
@@ -32,26 +32,29 @@ const Blog = () => {
 
       <div className="text-center w-full sm:w-3/4">
         <div className="relative">
-          <div className="w-12 mx-auto h-1 bg-light-orange mb-2"></div>
-          <h1 className="text-2xl sm:text-4xl text-gray-light font-bold font-custom">
-            {"<blog posts/>"}
+          <h1 className="text-2xl mt-10 sm:text-4xl text-light-orange  font-custom tracking-wider uppercase">
+            {"BLOG POSTS"}
           </h1>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-20 sm:mt-40">
           {posts.map((post) => (
             <div
               key={post.id}
-              className="p-4 bg-gray-dark1 rounded text-center"
+              className="p-6 bg-gray-dark1 rounded text-center shadow-md transition-all duration-500 ease-in-out transform hover:scale-105"
             >
               <img
                 src={post.image}
                 alt={post.title}
-                className="w-full h-64 object-cover rounded"
+                className="w-full h-64 object-cover rounded-t-lg"
               />
-              <p className="mt-4 text-light-orange font-medium">{post.date}</p>
-              <h2 className="text-2xl text-gray-light font-bold font-custom mt-2">
-                {post.title}
-              </h2>
+              <div className="p-4">
+                <p className="mt-4 text-light-orange font-medium">
+                  {post.date}
+                </p>
+                <h2 className="text-2xl text-gray-light font-bold font-custom mt-2">
+                  {post.title}
+                </h2>
+              </div>
             </div>
           ))}
         </div>
--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,24 +1,60 @@
 import React, { useEffect, useRef, useState } from "react";
 import Cube from "../Cube";
+import Project from "../Project";
+interface ProjectData {
+  id: number;
+  title: string;
+  date: string;
+  technologies: string[];
+}
 
 const Projects = () => {
-  const projectData = [
-    { id: 1, title: "Nonstop Dogwear", date: "2022-07-12" },
-    { id: 2, title: "Recepie WebSite", date: "2022-08-15" },
-    { id: 3, title: "Ida Dashboard", date: "2022-09-01" },
-    { id: 4, title: "Ida Store", date: "2022-09-01" },
+  const projectData: ProjectData[] = [
+    {
+      id: 1,
+      title: "Nonstop Dogwear",
+      date: "2022-07-12",
+      technologies: ["NextJs", "Prisma", "GraphQL"],
+    },
+    {
+      id: 2,
+      title: "Recepie WebSite",
+      date: "2022-08-15",
+      technologies: ["React", "Redux", "Firebase"],
+    },
+    {
+      id: 3,
+      title: "Ida Dashboard",
+      date: "2022-09-01",
+      technologies: ["Vue.js", "Vuetify", "Firestore"],
+    },
+    {
+      id: 4,
+      title: "Ida Store",
+      date: "2022-09-01",
+      technologies: ["Angular", "RxJs", "NGRX"],
+    },
   ];
 
   const scrollRef = useRef<HTMLDivElement | null>(null);
   const [isDragging, setIsDragging] = useState(false);
   const [startPosition, setStartPosition] = useState(0);
   const [scrollLeft, setScrollLeft] = useState(0);
-
+  const [selectedProject, setSelectedProject] = useState<null | {
+    id: number;
+    title: string;
+    date: string;
+    technologies: string[];
+  }>(null);
+  const [showSelectedProject, setShowSelectedProject] = useState(false);
   const onMouseDown = (e: any) => {
     setIsDragging(true);
     setStartPosition(e.pageX - scrollRef.current?.offsetLeft!);
     setScrollLeft(scrollRef.current?.scrollLeft!);
   };
+
+  console.log(showSelectedProject);
+  console.log(selectedProject);
 
   const onMouseMove = (e: any) => {
     if (!isDragging) return;
@@ -54,22 +90,27 @@ const Projects = () => {
         </div>
         <div
           ref={scrollRef}
-          className="flex mt-24 sm:mt-18 px-4 sm:px-8 overflow-x-scroll scrollbar-thin scrollbar-thumb-red-500 scrollbar-track-gray-800 space-x-1"
+          className="flex mt-20 sm:mt-18 px-4 sm:px-8 overflow-x-scroll scrollbar-thin scrollbar-thumb-red-500 scrollbar-track-gray-800 space-x-1"
           onMouseDown={onMouseDown}
           onMouseLeave={onMouseUp}
           onMouseUp={onMouseUp}
           onMouseMove={onMouseMove}
         >
-          {projectData.map(({ id, title, date }) => (
+          {projectData.map(({ id, title, date, technologies }) => (
             <div
               className="bg-gray-dark1 relative group cursor-pointer min-w-[25vw] flex-shrink-0 flex items-center justify-center"
               key={id}
               style={{ minHeight: "120px" }}
+              onClick={() => {
+                setSelectedProject({ id, title, date, technologies }),
+                  setShowSelectedProject(true); // This should be the correct function
+              }}
             >
               <img
                 src={`./images/project${id}.jpg`}
                 alt={title}
                 className="w-5/6 h-5/6 object-cover transition-all duration-500 ease-in-out transform group-hover:blur"
+                // set selected project
               />
               <div className="absolute inset-0 flex items-center justify-center transition-opacity duration-500 ease-in-out bg-black bg-opacity-0 opacity-0 group-hover:opacity-100 group-hover:bg-opacity-70">
                 <div className="text-center space-y-4">
@@ -90,6 +131,17 @@ const Projects = () => {
             </div>
           ))}
         </div>
+
+        {/* Show the selected project */}
+        {showSelectedProject && selectedProject && (
+          <Project
+            title={selectedProject.title}
+            date={selectedProject.date}
+            technologies={selectedProject.technologies}
+            setShowSelectedProject={setShowSelectedProject}
+            setSelectedProject={setSelectedProject}
+          />
+        )}
       </div>
     </div>
   );
